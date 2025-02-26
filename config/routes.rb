dd622ec17b982e1425a84f907adbde188c169730
EC2 Default User <ec2-user@ip-172-31-1-16.ap-northeast-1.compute.internal>
2025-02-26 06:07:09 +0000

    [Set excavation resources]

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   devise_for :customers
 
   root to: "homes#top"
@@ -8,6 +9,7 @@ Rails.application.routes.draw do
   resources :posts
   resources :customers, only: [:show, :edit, :update]
   resource :favorite, only: [:create, :destroy]
+  resources :excavations, only: [:new, :create]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
