88b652ca8ade94f8fce75f38e724b563b2d4e96f
EC2 Default User <ec2-user@ip-172-31-1-16.ap-northeast-1.compute.internal>
2025-02-26 04:25:44 +0000

    [Set fevorite model]

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,7 +5,9 @@ class Customer < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   has_one_attached :profile_image
+
 
   def get_profile_image(width, height)
     unless profile_image.attached?
