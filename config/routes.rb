03f3a3e4f1951361343f25301603e4548c9c7b47
EC2 Default User <ec2-user@ip-172-31-1-16.ap-northeast-1.compute.internal>
2025-02-26 04:26:37 +0000

    [Set favorite resource]

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,12 @@ Rails.application.routes.draw do
 
   root to: "homes#top"
   get 'homes/about' => 'homes#about', as: 'about'
+  get '/mypage' => 'customers#mypage', as: 'mypage'
+
   resources :posts
   resources :customers, only: [:show, :edit, :update]
-  get '/mypage' => 'customers#mypage', as: 'mypage'
+  resource :favorite, only: [:create, :destroy]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
