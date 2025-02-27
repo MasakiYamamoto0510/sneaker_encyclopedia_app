2dd03814f16067f9d2e42c2014c2a23b64f9fddf
EC2 Default User <ec2-user@ip-172-31-1-16.ap-northeast-1.compute.internal>
2025-02-27 10:05:20 +0000

    [Set Cancellation processing function]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :customer_state, only: [:create]
 
   def after_sign_in_path_for(resource)
     about_path
@@ -13,5 +14,17 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  private
+  def customer_state
+    customer = Customer.find_by(email: params[:customer][:email])
+    return if customer.nil?
+    return unless customer.valid_password?(params[:customer][:password])
+    if customer.is_active
+      return
+    else
+      redirect_to new_customer_registration_path
+    end
   end
 end
