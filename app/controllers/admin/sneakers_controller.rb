1af25cdeca0ee2d02f9f6043cc9aabf7ad737896
EC2 Default User <ec2-user@ip-172-31-1-16.ap-northeast-1.compute.internal>
2025-03-07 10:41:58 +0000

    [Add new sneaker functions]

--- app/controllers/admin/sneakers_controller.rb
+++ app/controllers/admin/sneakers_controller.rb
@@ -1,5 +1,13 @@
 class Admin::SneakersController < ApplicationController
   def new
+    @sneaker = Sneaker.new
+  end
+
+  def create
+    @sneaker = Sneaker.new(sneaker_params)
+    #@sneaker.admin_id = current_admin.id
+    @sneaker.save
+    redirect_to admin_sneaker_path(@sneaker.id)
   end
 
   def index
@@ -7,4 +15,11 @@ class Admin::SneakersController < ApplicationController
 
   def show
   end
+
+  private
+
+  def sneaker_params 
+    params.require(:sneaker).permit(:image, :brand_id, :sneaker_name, :overview, :year_of_manufacture, :color, :size_sex, :size_country, :numerical_size)
+  end
+
 end
--- app/models/sneaker.rb
+++ app/models/sneaker.rb
@@ -1,4 +1,12 @@
 class Sneaker < ApplicationRecord
   has_one_attached :image
   belongs_to :admin
+
+  def get_sneaker_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      sneaker_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpg')
+    end
+    sneaker_image.variant(resize_to_limit: [width,height]).processed
+  end
 end
