37de87e74e130318e9430123ea35ce57d298ae84
EC2 Default User <ec2-user@ip-172-31-1-16.ap-northeast-1.compute.internal>
2025-04-23 06:36:01 +0000

    [Refactoring sneaker_brand & type]

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,13 @@ module ApplicationHelper
     return 'customer' if customer_signed_in?
     nil
   end
+
+  def sneaker_link(sneaker)
+    if params[:controller].start_with?("admin/")
+      admin_sneaker_path(sneaker)
+    else
+      sneaker_path(sneaker)
+    end
+  end
+  
 end
