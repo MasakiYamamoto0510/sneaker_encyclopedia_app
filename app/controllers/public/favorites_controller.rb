8e57880c2ab129fbd53ae1f2cfb883ce1bdeb8c4
EC2 Default User <ec2-user@ip-172-31-1-16.ap-northeast-1.compute.internal>
2025-05-03 02:28:04 +0000

    [Add Ajax]

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,16 +1,21 @@
 class Public::FavoritesController < ApplicationController
+  before_action :set_sneaker
 
   def create
-    @sneaker = Sneaker.find(params[:sneaker_id])
     current_customer.favorites.find_or_create_by(sneaker_id: @sneaker.id)
     render 'public/favorites/toggle'
   end
 
   def destroy
-    @sneaker = Sneaker.find(params[:sneaker_id])
     favorite = current_customer.favorites.find_by(sneaker_id: @sneaker.id)
     favorite&.destroy
     render 'public/favorites/toggle'
+  end
+
+  private
+
+  def set_sneaker
+    @sneaker = Sneaker.find(params[:sneaker_id])
   end
 
 end
--- app/controllers/public/sneaker_comment_favorites_controller.rb
+++ app/controllers/public/sneaker_comment_favorites_controller.rb
@@ -1,20 +1,25 @@
 class Public::SneakerCommentFavoritesController < ApplicationController
+  before_action :set_comment
 
   def create
-    comment = SneakerComment.find(params[:sneaker_comment_id])
-    favorite = comment.sneaker_comment_favorites.find_or_create_by(customer_id: current_customer.id)
-    favorite.save
-    flash[:success] = "”いいね”！"
-    redirect_to sneaker_path(comment.sneaker_id)
-    
+    @favorite = @comment.sneaker_comment_favorites
+                        .find_or_create_by(customer: current_customer)
+    respond_to do |format|
+      format.js { render :toggle }
+    end
   end
 
   def destroy
-    comment = SneakerComment.find(params[:sneaker_comment_id])
-    favorite = comment.sneaker_comment_favorites.find_by(customer_id: current_customer.id)
-    favorite&.destroy 
-    flash[:danger] = "”いいね”を取り消しました。"
-    redirect_to sneaker_path(comment.sneaker_id)
+    @favorite = @comment.sneaker_comment_favorites.find_by(customer: current_customer)
+    @favorite&.destroy 
+    respond_to do |format|
+      format.js { render :toggle }
+    end
   end
 
+  private
+
+  def set_comment
+    @comment = SneakerComment.find(params[:sneaker_comment_id])
+  end
 end
