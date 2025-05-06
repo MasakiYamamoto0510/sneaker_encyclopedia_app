db60f256aef68da9f197fc573894609c5c8f3f91
EC2 Default User <ec2-user@ip-172-31-1-16.ap-northeast-1.compute.internal>
2025-05-06 04:25:41 +0000

    [Set Ajax conversion of コメントいいねボタン]

--- app/controllers/public/sneaker_comment_favorites_controller.rb
+++ app/controllers/public/sneaker_comment_favorites_controller.rb
@@ -1,19 +1,25 @@
 class Public::SneakerCommentFavoritesController < ApplicationController
+  before_action :set_comment
 
   def create
-    comment = SneakerComment.find(params[:sneaker_comment_id])
-    favorite = comment.sneaker_comment_favorites.new(customer_id: current_customer.id)
-    favorite.save
-    flash[:success] = "”いいね”！"
-    redirect_to sneaker_path(comment.sneaker_id)
+    current_customer.sneaker_comment_favorites.find_or_create_by!(sneaker_comment: @comment)
+    respond_to do |format|
+      format.js   
+      format.html { redirect_back fallback_location: sneaker_path(@comment.sneaker) }
+    end
   end
 
   def destroy
-    comment = SneakerComment.find(params[:sneaker_comment_id])
-    favorite = comment.sneaker_comment_favorites.find_by(customer_id: current_customer.id)
-    favorite.destroy 
-    flash[:danger] = "”いいね”を取り消しました。"
-    redirect_to sneaker_path(comment.sneaker_id)
+    current_customer.sneaker_comment_favorites.find_by(sneaker_comment: @comment)&.destroy
+    respond_to do |format|
+      format.js   
+      format.html { redirect_back fallback_location: sneaker_path(@comment.sneaker) }
+    end
+  end
+
+  private
+  def set_comment
+    @comment = SneakerComment.find(params[:sneaker_comment_id])
   end
 
 end
