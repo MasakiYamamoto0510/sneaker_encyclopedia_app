54802e6faeb2b3053eb51ace164c9faa7dd2ca90
EC2 Default User <ec2-user@ip-172-31-1-16.ap-northeast-1.compute.internal>
2025-05-20 06:38:20 +0000

    [Refactor(routes.rb) Reorganized & commented to make them easier to read]

--- config/routes.rb
+++ config/routes.rb
@@ -1,67 +1,82 @@
 Rails.application.routes.draw do
-  
-  namespace :admin do
-    get 'contacts/index'
-    get 'contacts/show'
-    get 'contacts/destroy'
-  end
+  # --　管理者用ルーティング　--------------------------------------------------------------
   devise_for :admins, path: 'admin', skip: [:registrations, :password], controllers: {
     sessions: 'admin/sessions'
   }
 
   namespace :admin do
+    # ダッシュボード
     get 'dashboards', to: 'dashboards#index'
-    get '/dashboards/sneaker', to: 'dashboards#sneaker'
-    get '/search' => 'searches#search', as: 'search'
+    get 'dashboards/sneaker', to: 'dashboards#sneaker'
+    get 'search', to: 'searches#search', as: 'search'
 
-    resources :customers, only: [:show, :index, :destroy]
-    resources :posts, only: [:show, :index, :destroy]
+    # リソース
+    resources :customers, only: [:index, :show, :destroy]
+    resources :posts, only: [:index, :show, :destroy]
     resources :admins, only: [:show, :edit, :update]
     resources :sneakers
     resources :sneaker_comments, only: [:destroy]
+    
     resources :contacts, only: [:index, :show, :destroy] do
       member do
         patch :mark_read
         patch :mark_unread
       end
     end
+
     resources :sneaker_brands do
       resources :sneaker_types, only: [:new, :create, :show, :edit, :update, :destroy]
     end
   end
- 
-  scope module: :public do
 
+  # -- 一般ユーザー用ルーティング ----------------------------------------------------------------
+  scope module: :public do
+    # Devise（Customer）
     devise_for :customers, skip: [:passwords], controllers: {
       registrations: "public/registrations",
       sessions: 'public/sessions'
     }
 
     devise_scope :customer do
-      post "customers/guest_sign_in", to: "sessions#guest_sign_in", as: :customers_guest_sign_in
+      post 'customers/guest_sign_in', to: 'sessions#guest_sign_in', as: :customers_guest_sign_in
     end
 
+    # 静的ページ
     root to: 'homes#top'
-    get 'homes/about' => 'homes#about', as: 'about'
-    get '/mypage' => 'customers#mypage', as: 'mypage'
-    patch '/mypage' => 'customers#update', as: 'update_mypage'
-    post '/sneakers/new' => 'sneakers#create'
+    get 'homes/about', to: 'homes#about', as: 'about'
 
-    get '/customers/:id/unsubscribe' => 'customers#unsubscribe', as: 'unsubscribe'
-    patch '/customers/:id/withdrawal' => 'customers#withdrawal', as: 'withdrawal'
-    get '/search' => 'searches#search', as: 'search'
+    # マイページ
+    get 'mypage', to: 'customers#mypage', as: 'mypage'
+    patch 'mypage', to: 'customers#update', as: 'update_mypage'
 
+    # 退会関連
+    get 'customers/:id/unsubscribe', to: 'customers#unsubscribe', as: 'unsubscribe'
+    patch 'customers/:id/withdrawal', to: 'customers#withdrawal', as: 'withdrawal'
+
+    # 検索
+    get 'search', to: 'searches#search', as: 'search'
+
+    # 投稿
     resources :posts
+
+    # 顧客
     resources :customers, only: [:show, :edit, :update]
-    resource :sneaker_comment_favorites, only: [:create, :destroy]
-    resources :sneakers, only: [:index, :show] do
+
+    # スニーカー
+    resources :sneakers, only: [:index, :show, :create] do
       resource :favorite, only: [:create, :destroy]
       resources :sneaker_comments, only: [:create, :destroy]
     end
+
+    # コメントのお気に入り
+    resource :sneaker_comment_favorites, only: [:create, :destroy]
+
+    # ブランド・タイプ
     resources :sneaker_brands, only: [:index, :show] do
       resources :sneaker_types, only: [:index, :show]
     end
 
+    # お問い合わせ
     resources :contacts, only: [:new, :create] do
       collection do
         post 'confirm'
@@ -69,6 +84,5 @@ Rails.application.routes.draw do
         get 'done'
       end
     end
-    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  end 
+  end
 end
